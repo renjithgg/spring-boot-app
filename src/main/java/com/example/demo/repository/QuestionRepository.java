4444d93703bcc37ddae49fdec88860dd5bb4cca0
renjithgg <renjithgg@gmail.com>
2019-08-05 10:30:10 +0530

    Update: Add & retrieve question

--- src/main/java/com/example/demo/repository/QuestionRepository.java
+++ src/main/java/com/example/demo/repository/QuestionRepository.java
@@ -1,11 +1,11 @@
 package com.example.demo.repository;
 
-import com.example.demo.model.Renjith;
+import com.example.demo.model.Question;
 import org.springframework.data.repository.CrudRepository;
 
 // This will be AUTO IMPLEMENTED by Spring into a Bean called userRepository
 // CRUD refers Create, Read, Update, Delete
 
-public interface RenjithRepository extends CrudRepository<Renjith, Integer> {
+public interface QuestionRepository extends CrudRepository<Question, Integer> {
 
 }
